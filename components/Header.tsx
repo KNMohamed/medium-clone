1eb3acf2338517fac7c32cf1595acb22fed74252
yourmaingotoguy <khalid.n.mohamed@outlook.com>
2022-03-10 13:16:08 -0500

    Finished top nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import Logo from './Logo'
 function Header() {
   return (
-    <header>
+    <header className="mx-auto flex max-w-7xl justify-between p-5">
       <div className="flex items-center space-x-5">
         <Link href="/">
           <Logo />
@@ -11,8 +11,16 @@ function Header() {
         <div className="hidden items-center space-x-5 md:inline-flex">
           <h3>About</h3>
           <h3>Contact</h3>
-          <h3>Follow</h3>
+          <h3 className="rounded-full bg-green-600 px-4 py-1 text-white">
+            Follow
+          </h3>
         </div>
+      </div>
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sign In</h3>
+        <h3 className="rounded-full border-2 border-green-600 px-4 py-1">
+          Get Started
+        </h3>
       </div>
     </header>
   )
